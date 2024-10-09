36b98315b6f09b71f7db8650adad13ab352a0f5b
s1lvax <s1lvax@proton.me>
2024-10-09 18:52:50 +0200

    Added Prisma config and adds user and checks if they exist once in /profile

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -3,10 +3,20 @@ import type { PageServerLoad } from './$types';
 import { countGithubProjects } from '$lib/utils/countGithubProjects';
 import { getGithubUsername } from '$lib/utils/getGithubUsername';
 import { fetchGitHubContributionsCount } from '$lib/utils/fetchGitHubContributions'; // Import the new function
+import { getGitHubUserIdFromImageUrl } from '$lib/utils/getGithubIDFromImage';
+import { prisma } from '$lib/server/prisma';
 
 export const load: PageServerLoad = async (event) => {
 	const session = await event.locals.auth();
 	if (!session?.user) throw redirect(303, '/');
+
+	// Extract user ID from the image URL
+	const userId = getGitHubUserIdFromImageUrl(session.user.image);
+
+	// Check if user ID is defined
+	if (!userId) {
+		throw new Error('User ID could not be determined from image URL');
+	}
 
 	// Get the GitHub username asynchronously
 	const username = await getGithubUsername(session.user.image);
@@ -14,6 +24,21 @@ export const load: PageServerLoad = async (event) => {
 	// Check if username is defined
 	if (!username) {
 		throw new Error('Username could not be determined from image URL');
+	}
+
+	// Check if the user exists in the database
+	let user = await prisma.user.findUnique({
+		where: { githubId: userId } // Use the extracted userId
+	});
+
+	// If user does not exist, create a new user record
+	if (!user) {
+		user = await prisma.user.create({
+			data: {
+				githubId: userId,
+				githubUsername: username
+			}
+		});
 	}
 
 	// Fetch the repository count using the username
