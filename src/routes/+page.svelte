7600d9b79c8c51a2c65df917b3363d6811600477
s1lva <s1lvax@proton.me>
2024-10-10 14:51:24 +0200

    Public profile availbale

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Features from '$lib/components/Features.svelte';
 	import Introduction from '$lib/components/Introduction.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
+<Navbar />
 <Introduction />
 <Features />
---
+++ src/routes/[username]/+page.server.ts
@@ -0,0 +1,49 @@
+import { prisma } from '$lib/server/prisma';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+	const username = params.username;
+
+	if (!username) {
+		throw new Error('Username is required');
+	}
+
+	const user = await prisma.user.findUnique({
+		where: { githubUsername: username }
+	});
+
+	if (!user) {
+		throw new Error('User not found');
+	}
+
+	//update view by one
+
+	await prisma.user.update({
+		where: { githubId: user.githubId },
+		data: {
+			views: user.views + 1
+		}
+	});
+
+	const links = await prisma.link.findMany({
+		where: { userId: user.githubId }
+	});
+
+	const skills = await prisma.skill.findMany({
+		where: { userId: user.githubId }
+	});
+
+	const userData = {
+		links,
+		skills,
+		repoCount: user.repoCount,
+		contributionsCount: user.contributionsCount,
+		pfp: user.pfp,
+		praises: user.praises,
+		username: username
+	};
+
+	return {
+		userData
+	};
+};
