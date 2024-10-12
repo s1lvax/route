4da83a276a02ce5ffd9b54fb4e606894847be424
s1lvax <s1lvax@proton.me>
2024-10-12 12:16:13 +0200

    make sure every link starts with http:// or https:// / fix #14

--- src/lib/schemas/links.ts
+++ src/lib/schemas/links.ts
@@ -2,7 +2,13 @@ import { z } from 'zod';
 
 export const linksSchema = z.object({
 	title: z.string().min(1).max(50),
-	url: z.string().min(10).max(200)
+	url: z
+		.string()
+		.min(10)
+		.max(200)
+		.refine((val) => val.startsWith('http://') || val.startsWith('https://'), {
+			message: 'Oops! URLs usually start with http:// or https:// :P'
+		})
 });
 
 export type LinksSchema = typeof linksSchema;
