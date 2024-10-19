ad4a830203b9bb40a826f46888002444e0b93ef1
glitchingcore <102431570+glitchingcore@users.noreply.github.com>
2024-10-19 16:20:25 +0200

    Fix bug with DnD type and use ES6 arrow functions

--- src/lib/components/Shared/DnD.svelte
+++ src/lib/components/Shared/DnD.svelte
@@ -6,18 +6,19 @@
     export let updateNewItems: (newItems: any[]) => void;
     export let onDrop: ((e: CustomEvent<DndEvent<Item>>) => void) | undefined = undefined;
     export let containerTag: string = 'div';
-    export let dndOptions: Omit<Options<Item>, 'items'> = {};
-    dndOptions.type = dndOptions.type || crypto.randomUUID(); // By default DnD will have different types, to prevent dragging between DnD zones.
+    export let dndOptions: Omit<Options<Item>, 'items'|'type'> = {};
+    export let type: string = crypto.randomUUID(); // By default DnD will have different types, to prevent dragging between DnD zones.
 
-    function handleDndConsider(e: CustomEvent<DndEvent<Item>>) {
+
+    const handleDndConsider = (e: CustomEvent<DndEvent<Item>>) => {
         updateNewItems(e.detail.items)
     }
-    function handleDndFinalize(e: CustomEvent<DndEvent<Item>>) {
+    const handleDndFinalize = (e: CustomEvent<DndEvent<Item>>) => {
         updateNewItems(e.detail.items)
         onDrop && onDrop(e);
     }
 </script>
 
-<svelte:element this="{containerTag}" use:dndzone="{{ ...dndOptions, items }}" on:consider="{handleDndConsider}" on:finalize="{handleDndFinalize}" {...$$restProps}>
+<svelte:element this="{containerTag}" use:dndzone="{{ ...dndOptions, items, type }}" on:consider="{handleDndConsider}" on:finalize="{handleDndFinalize}" {...$$restProps}>
     <slot></slot>
 </svelte:element>
