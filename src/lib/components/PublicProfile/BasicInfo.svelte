0502768b0d6079c9dcf6cb222969b12d6f78b170
Discusser <47938380+Discusser@users.noreply.github.com>
2024-10-21 14:58:08 +0200

    Add skeletons for BasicInfo

--- src/lib/components/PublicProfile/BasicInfo.svelte
+++ src/lib/components/PublicProfile/BasicInfo.svelte
@@ -3,58 +3,76 @@
 	import { Button } from '$lib/components/ui/button';
 	import { IdCard } from 'lucide-svelte';
 	import GitHub from 'lucide-svelte/icons/github';
-
+	import { Skeleton } from '$lib/components/ui/skeleton';
 	import type { GithubData } from '$lib/types/GithubData';
 	import type { PublicProfile } from '$lib/types/PublicProfile';
-	export let githubData: GithubData;
+
+	export let githubData: GithubData | null;
 	export let userData: PublicProfile;
 </script>
 
 <!-- User Avatar and Basic Info -->
 <div class="flex items-center justify-center space-x-4">
 	<Avatar.Root class="h-24 w-24 rounded-full">
-		<Avatar.Image src={githubData.avatarUrl} alt="User Avatar" />
-		<Avatar.Fallback>?</Avatar.Fallback>
+		{#if githubData}
+			<Avatar.Image src={githubData.avatarUrl} alt="User Avatar" />
+			<Avatar.Fallback>?</Avatar.Fallback>
+		{:else}
+			<Skeleton class="h-full w-full rounded-full"></Skeleton>
+		{/if}
 	</Avatar.Root>
 	<div class="flex flex-col space-y-4 text-center">
-		{#if githubData.name}
-			<p class="text-2xl font-bold">{githubData.name}</p>
-		{:else}
-			<p class="text-xl font-bold">{userData.username}</p>
-		{/if}
+		{#if githubData}
+			{#if githubData.name}
+				<p class="text-2xl font-bold">{githubData.name}</p>
+			{:else}
+				<p class="text-2xl font-bold">{userData.username}</p>
+			{/if}
 
-		{#if githubData.bio}
-			<p class="text-muted-foreground">{githubData.bio}</p>
-		{:else}
-			<p class="text-muted-foreground">Public Developer Profile</p>
-		{/if}
+			{#if githubData.bio}
+				<p class="text-muted-foreground">{githubData.bio}</p>
+			{:else}
+				<p class="text-muted-foreground">Public Developer Profile</p>
+			{/if}
 
-		{#if githubData.company}
-			<p class="text-muted-foreground">Currently at {githubData.company}</p>
-		{/if}
-		<div class="flex flex-row items-center justify-center gap-4">
-			<div>
-				<Button
-					href={githubData.url}
-					target="_blank"
-					class="mt-2 flex justify-center rounded-full"
-					variant="outline"
-				>
-					<GitHub />
-				</Button>
-			</div>
-			{#if githubData.blog}
+			{#if githubData.company}
+				<p class="text-muted-foreground">Currently at {githubData.company}</p>
+			{/if}
+			<div class="flex flex-row items-center justify-center gap-4">
 				<div>
 					<Button
-						href={githubData.blog}
+						href={githubData.url}
 						target="_blank"
 						class="mt-2 flex justify-center rounded-full"
 						variant="outline"
 					>
-						<IdCard />
+						<GitHub />
 					</Button>
 				</div>
-			{/if}
-		</div>
+				{#if githubData.blog}
+					<div>
+						<Button
+							href={githubData.blog}
+							target="_blank"
+							class="mt-2 flex justify-center rounded-full"
+							variant="outline"
+						>
+							<IdCard />
+						</Button>
+					</div>
+				{/if}
+			</div>
+		{:else}
+			<div class="flex w-[300px] justify-center">
+				<Skeleton class="h-8 w-[75px]"></Skeleton>
+			</div>
+			<Skeleton class="h-6 w-[300px]"></Skeleton>
+
+			<div class="flex flex-row items-center justify-center gap-4">
+				{#each { length: 2 } as _}
+					<Skeleton class="mt-2 h-[40px] w-[58px] rounded-full"></Skeleton>
+				{/each}
+			</div>
+		{/if}
 	</div>
 </div>
