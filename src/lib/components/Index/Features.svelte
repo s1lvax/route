be881183a0f4006fb6ed30569b51e1d0d64e27d5
s1lvax <s1lvax@proton.me>
2024-10-21 21:26:18 +0200

    changed the index website

--- src/lib/components/Index/Features.svelte
+++ src/lib/components/Index/Features.svelte
@@ -1,23 +1,38 @@
 <script>
-	import NewSection from '$lib/components/Shared/NewSection.svelte';
 	import { features } from '$lib/constants/features';
-	import * as Card from '$lib/components/ui/card';
 </script>
 
-<NewSection>
-	<div class="grid grid-cols-1 gap-8 lg:grid-cols-2">
-		{#each features as feature}
-			<Card.Root>
-				<Card.Header>
-					<Card.Title class="flex items-center space-x-2">
-						<svelte:component this={feature.icon} />
-						<span>{feature.name}</span>
-					</Card.Title>
-				</Card.Header>
-				<Card.Content>
-					<Card.Description>{feature.description}</Card.Description>
-				</Card.Content>
-			</Card.Root>
-		{/each}
+<div class=" py-24 sm:py-32">
+	<div class="mx-auto max-w-7xl px-6 lg:px-8">
+		<div class="mx-auto max-w-2xl lg:mx-0">
+			<h2 class="text-pretty text-4xl font-semibold tracking-tight sm:text-5xl">
+				Unleash the Power of Your Developer Profile
+			</h2>
+			<p class="mt-6 text-lg leading-8 text-muted-foreground">
+				Designed to showcase your skills, projects, and contributions effortlessly, Route empowers
+				developers to tell their story with ease.
+			</p>
+		</div>
+		<div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+			<dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+				{#each features as feature}
+					<div class="flex flex-col">
+						<dt class="text-base font-semibold leading-7">
+							<div
+								class="mb-6 flex h-10 w-10 items-center justify-center rounded-lg border border-gray-800 bg-transparent"
+							>
+								<svelte:component this={feature.icon} />
+							</div>
+							{feature.name}
+						</dt>
+						<dd class="mt-1 flex flex-auto flex-col text-base leading-7 text-muted-foreground">
+							<p class="flex-auto">
+								{feature.description}
+							</p>
+						</dd>
+					</div>
+				{/each}
+			</dl>
+		</div>
 	</div>
-</NewSection>
+</div>
---
+++ src/lib/components/Index/Hero.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { GitPullRequestCreate, UserPen, Github, LogOut } from 'lucide-svelte';
+
+	import { signIn, signOut } from '@auth/sveltekit/client';
+	import { page } from '$app/stores';
+</script>
+
+<div class="relative isolate overflow-hidden">
+	<div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+		<div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:pt-8">
+			<div class="mt-24 sm:mt-32 lg:mt-16">
+				<a href="https://github.com/s1lvax/route" class="inline-flex space-x-6">
+					<span
+						class="rounded-full px-3 py-1 text-sm font-semibold leading-6 ring-1 ring-inset ring-indigo-500/20"
+						>What's new</span
+					>
+					<span class="inline-flex items-center space-x-2 text-sm font-medium leading-6">
+						<span>Just shipped v1.0.0</span>
+						<svg
+							class="h-5 w-5 text-gray-500"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+							data-slot="icon"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</span>
+				</a>
+			</div>
+			<h1 class="mt-10 text-pretty text-5xl font-semibold tracking-tight sm:text-7xl">
+				Your Developer Identity, Streamlined and Simplified
+			</h1>
+			<p class="text-md mt-8 text-pretty text-muted-foreground sm:text-xl/8">
+				Create a profile that highlights your GitHub stats and key projects—simple, clean, and
+				yours.
+			</p>
+			<div class="mt-10 flex items-center gap-x-6">
+				{#if $page.data.session}
+					<Button href="/profile" class="flex items-center space-x-2"
+						><UserPen /> <span>My Profile</span></Button
+					>
+					<Button
+						on:click={() => signOut()}
+						variant="destructive"
+						class="flex items-center space-x-2"><LogOut /> <span>Logout</span></Button
+					>
+				{:else}
+					<Button on:click={() => signIn('github')} class="flex items-center space-x-2"
+						><Github /> <span>Sign in with Github</span></Button
+					>
+				{/if}
+			</div>
+		</div>
+		<div
+			class="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32"
+		>
+			<div class="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+				<img
+					src="https://i.imgur.com/KtVWxtM.png"
+					alt="App screenshot"
+					width="2432"
+					height="1442"
+					class="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
+				/>
+			</div>
+		</div>
+	</div>
+</div>
