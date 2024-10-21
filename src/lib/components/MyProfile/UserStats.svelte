3f3fc5af89a3141b0d7c6c510353714bc87ec587
Discusser <47938380+Discusser@users.noreply.github.com>
2024-10-21 15:12:01 +0200

    Refactor and skeletons for public profile

--- src/lib/components/MyProfile/UserStats.svelte
+++ src/lib/components/MyProfile/UserStats.svelte
@@ -5,50 +5,37 @@
 	import * as Card from '$lib/components/ui/card';
 	import type { PrivateProfileData } from '$lib/types/PrivateProfileData';
 	import { Skeleton } from '$lib/components/ui/skeleton';
+	import StatsCard from '$lib/components/Shared/StatsCard.svelte';
+	import StatsCardSkeleton from '../Shared/StatsCardSkeleton.svelte';
 
 	export let privateProfileData: PrivateProfileData | null;
-
-	// Each card has exactly the same structure, so we might aswell avoid writing the same HTML each time
-	const cards = [
-		{ icon: Users, title: 'Total Profile Views', data: (p: PrivateProfileData) => p.views },
-		{ icon: Folder, title: 'Projects on Github', data: (p: PrivateProfileData) => p.repoCount },
-		{
-			icon: GitHub,
-			title: 'GitHub Contributions (Past 30 Days)',
-			data: (p: PrivateProfileData) => p.contributionsCount
-		},
-		{ icon: GitHub, title: 'Github Followers', data: (p: PrivateProfileData) => p.followers }
-	];
 </script>
 
 <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
 	{#if privateProfileData}
-		{#each cards as card}
-			<Card.Root>
-				<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
-					<div class="flex items-center space-x-2">
-						<svelte:component this={card.icon} class="h-4 w-4 text-muted-foreground"
-						></svelte:component>
-						<Card.Title class="text-sm font-medium">{card.title}</Card.Title>
-					</div>
-				</Card.Header>
-				<Card.Content>
-					<div class="text-2xl font-bold">{card.data(privateProfileData)}</div>
-				</Card.Content>
-			</Card.Root>
-		{/each}
+		<StatsCard
+			icon={Users}
+			title="Total Profile Views"
+			data={privateProfileData.views.toString()}
+		/>
+		<StatsCard
+			icon={Folder}
+			title="Projects on Github"
+			data={privateProfileData.repoCount.toString()}
+		/>
+		<StatsCard
+			icon={GitHub}
+			title="GitHub Contributions (Past 30 Days)"
+			data={privateProfileData.contributionsCount.toString()}
+		/>
+		<StatsCard
+			icon={GitHub}
+			title="GitHub Followers"
+			data={privateProfileData.followers.toString()}
+		/>
 	{:else}
-		{#each cards as _}
-			<Card.Root>
-				<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
-					<div class="flex w-full items-center space-x-2">
-						<Skeleton class="h-5 w-[75%]"></Skeleton>
-					</div>
-				</Card.Header>
-				<Card.Content>
-					<Skeleton class="h-8 w-[40%]"></Skeleton>
-				</Card.Content>
-			</Card.Root>
+		{#each { length: 4 } as _}
+			<StatsCardSkeleton />
 		{/each}
 	{/if}
 </div>
