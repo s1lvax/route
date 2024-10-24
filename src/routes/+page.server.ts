aacc7ebb6dea6fc937eeeef8acb9cdb30666094a
s1lvax <s1lvax@proton.me>
2024-10-24 21:50:14 +0200

    change logic

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,14 +2,11 @@ import { prisma } from '$lib/server/prisma';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	//fetch 6 random users
-	const userCount = await prisma.user.count();
-	const skip = Math.floor(Math.random() * userCount);
+	//fetch 5 users with the most views
 	const users = await prisma.user.findMany({
 		take: 6,
-		skip: skip,
 		orderBy: {
-			id: 'desc'
+			views: 'desc'
 		}
 	});
 
