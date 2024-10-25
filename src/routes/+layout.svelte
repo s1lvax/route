280374dbd6d7f4144a3ab87ae726a67d136e0d86
s1lvax <s1lvax@proton.me>
2024-10-25 15:01:19 +0200

    fix error

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,17 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { dev } from '$app/environment';
+	import { page } from '$app/stores';
+	import { derived } from 'svelte/store';
 
-	// Check if the current URL hostname is route2.dev
-	const isRoute2Dev = !dev && window?.location.hostname === 'route2.dev';
+	// Create a derived store to check if the hostname is route2.dev
+	const isRoute2Dev = derived(page, ($page) => !dev && $page.url.hostname === 'route2.dev');
 
 	const tokenString = '"token": "c0d92e4dcb43400c89c094b727fe5026"';
 </script>
 
 <svelte:head>
-	{#if isRoute2Dev}
+	{#if $isRoute2Dev}
 		<!-- Cloudflare Web Analytics -->
 		<script
 			defer
