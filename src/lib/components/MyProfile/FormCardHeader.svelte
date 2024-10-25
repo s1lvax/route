e92ff6a88ed497741aa7d97345ee62bfd29d1140
Discusser <47938380+Discusser@users.noreply.github.com>
2024-10-25 15:36:29 +0200

    Change implementation for toggle transition on forms

--- src/lib/components/MyProfile/FormCardHeader.svelte
+++ src/lib/components/MyProfile/FormCardHeader.svelte
@@ -6,21 +6,9 @@
 	export let description: string;
 
 	let showForm: boolean = false;
-	let wrapper: HTMLDivElement | null;
 
-	// Transitioning from a fixed height (0px) to a variable height with CSS is not exactly possible with most browsers
-	// at the moment of writing this.
-	// There is an experimental CSS property (interpolate-size) that aims to fix this but it's not available on many
-	// browsers. For now I have implemented this using JS (https://stackoverflow.com/a/3149710)
 	const toggleVisibility = () => {
-		if (!wrapper) return;
-
 		showForm = !showForm;
-		if (showForm) {
-			wrapper.style.height = wrapper.scrollHeight + 'px';
-		} else {
-			wrapper.style.height = '0';
-		}
 	};
 </script>
 
@@ -34,8 +22,10 @@
 			<span class="text-xl">{showForm ? '-' : '+'}</span>
 		</Button>
 	</div>
-	<div class="flex flex-col">
-		<div bind:this={wrapper} class="h-0 overflow-hidden transition-all">
+	<div
+		class="grid {showForm ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'} transition-[grid-template-rows]"
+	>
+		<div class="overflow-hidden">
 			<slot></slot>
 		</div>
 	</div>
