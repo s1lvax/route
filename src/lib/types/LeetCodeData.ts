410eb4f4f2177b17fb742baf7f69dd9065ffa420
omaramkotb22 <omaramkotb22@gmail.com>
2024-11-10 02:33:27 +0400

    Refactor LeetCodeData types to include user profile and global submission statistics

--- src/lib/types/LeetCodeData.ts
+++ src/lib/types/LeetCodeData.ts
@@ -1,21 +1,30 @@
 export type LeetCodeStats = {
     username: string;
     userCalendar: {
-       activeYears: number[];
-       streak: number;
-       totalActiveDays: number;
-       dccBadges: {
-          timestamp: string;
-          badge: {
-             name: string;
-             icon: string;
-          };
-       }[];
-       submissionCalendar: string;
+        activeYears: number[];
+        streak: number;
+        totalActiveDays: number;
+        dccBadges: {
+            timestamp: string;
+            badge: {
+                name: string;
+                icon: string;
+            };
+        }[];
     };
- };
- 
- export type LanguageStats = {
-    languageName: string;
+    profile: {
+        ranking: number;
+    };
+    submitStatsGlobal:{
+        acSubmissionNum: {
+            difficulty: string;
+            count: number;
+        }[];
+    }
+};
+
+export type TagProblem = {
+    tagName: string;
+    tagSlug: string;
     problemsSolved: number;
- };
+};
